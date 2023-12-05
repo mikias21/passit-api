7070d443b729db404d615816bf69ead492583056
mikias21 <mikiasmesfinA0123@gmail.com>
2023-12-06 00:56:36 +0800

    Quick fix

--- controller/signin_controller.py
+++ controller/signin_controller.py
@@ -3,6 +3,5 @@ from fastapi import Response, status
 from schemas.signin import Signin
 
 async def signin_controller(user: Signin):
-    # Check if either signup by email or phone is true
-    if not user.is_email and not user.is_phone:
-        return Response("AuthErrorMessages.NO_SIGNUP_METHOD.value", "status.HTTP_406_NOT_ACCEPTABLE")
+    
+    return Response("AuthErrorMessages.NO_SIGNUP_METHOD.value", status.HTTP_406_NOT_ACCEPTABLE)
