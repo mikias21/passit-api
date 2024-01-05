181e8908eeb240b6f279bbcf5a5bb4adcea66988
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-06 03:18:12 +0800

    In release mode

--- constants/general.py
+++ constants/general.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 class General(Enum):
-    RELEASE = False
+    RELEASE = True
     ALLOWED_TAGS = ['a', 'b', 'blockquote', 'br', 'em', 'i', 'li', 'ol', 'p', 'strong', 'ul']
     ALLOWED_ATTRIBUTES = {'a': ['href', 'title'], 'blockquote': ['cite']}
