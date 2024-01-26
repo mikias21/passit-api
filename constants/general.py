9e4e7ef9664e79a048b8141dc02a9ace459ddd11
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-27 05:41:11 +0800

    Release mode

--- constants/general.py
+++ constants/general.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class General(Enum):
-    RELEASE = False
+    RELEASE = True
     ALLOWED_TAGS = ['a', 'b', 'blockquote', 'br', 'em', 'i', 'li', 'ol', 'p', 'strong', 'ul']
     ALLOWED_ATTRIBUTES = {'a': ['href', 'title'], 'blockquote': ['cite']}
     LOCAL_URL = "http://localhost:3000"
